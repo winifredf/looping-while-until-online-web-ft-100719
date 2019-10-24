5f9dbd2945c77fbef7e9fb961be71c443774c171
Winifred Frierson <winifred.dykes@hotmail.com>
2019-10-24 11:05:35 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -11,7 +11,7 @@ end
 def using_while
   levitation_force = 6
   until levitation_force < 10
-  puts "Wingardium Leviosa"
-  levitation_force += 1
+    puts "Wingardium Leviosa"
+    levitation_force += 1
   end
 end
